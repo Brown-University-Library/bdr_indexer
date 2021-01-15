f1ef289dc9ba1f0facaec44876b9500512f491b1
Ben Cail <benjamin_cail@brown.edu>
2021-01-15 14:48:44 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bdr_solrizer",
-    version='3.9',
+    version='4.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     scripts=['bin/queue_solrize'],
