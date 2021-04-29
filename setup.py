48448b66ede7975f0dbf401a32d1de848a76e994
Joseph Rhoads <joseph_rhoads@brown.edu>
2021-04-29 15:43:18 -0400

    Bump Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bdr_solrizer",
-    version='4.7',
+    version='4.8',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     scripts=['bin/queue_solrize'],
