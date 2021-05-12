22d9b7c0d3fcc83614c7e1d92f7773a763c657c9
Ben Cail <benjamin_cail@brown.edu>
2021-05-12 13:03:10 -0400

    update setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bdr_solrizer",
-    version='4.8',
+    version='4.9',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     scripts=['bin/queue_solrize'],
