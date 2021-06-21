29a999fa4394593b36efbc6b0ddbd5a0a5c910e9
Ben Cail <benjamin_cail@brown.edu>
2021-06-21 17:01:21 -0400

    some fixes, and use rq.worker logger so our output goes to the same log as rq.worker logging

--- bdr_solrizer/logger.py
+++ bdr_solrizer/logger.py
@@ -1,14 +1,3 @@
 import logging
-import logging.handlers
 
-def setup_logger():
-    formatter = logging.Formatter(u'%(asctime)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger(u'solrizer_logger')
-    logger.propagate = False
-    logger.setLevel(logging.DEBUG)
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-    return logger
-
-logger = setup_logger()
+logger = logging.getLogger('rq.worker')
