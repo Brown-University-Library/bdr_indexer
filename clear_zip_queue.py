ee7ebd971434f3181eed65e6f90635fe583de5e8
Birkin J Diana <bdiana@dlibbdrcit.services.brown.edu>
2022-11-07 15:46:46 -0500

    converts queue specification to envar from hard-coded name

--- clear_zip_queue.py
+++ clear_zip_queue.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-import sys
+import os, sys
 
 from redis import Redis
 from rq import Queue
 
-ZIP_Q = Queue('index-zip', connection=Redis())
+QUEUE_NAME = os.environ['BDR_INDEXER__REDIS_QUEUE_NAME']
+
+# ZIP_Q = Queue('solrizer', connection=Redis())
+ZIP_Q = Queue(QUEUE_NAME, connection=Redis())
 total = ZIP_Q.count
 print(f'Total jobs: {total}')
 print('emptying queue...')
